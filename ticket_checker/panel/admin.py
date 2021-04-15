393c6a879a98c7921369f916172da595bcbef48e
Vladimir Serov <me@cab404.ru>
2021-04-15 23:48:50 +0300

    added qr export

--- ticket_checker/panel/admin.py
+++ ticket_checker/panel/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 from django.contrib.auth.models import Group
+import zipfile
+from qr_code.qrcode import maker as qrmaker
+from django.http.response import FileResponse
+import io
 
 # Register your models here.
 from .models import Ticket
 
+def download_stuff(modeladmin, request, queryset: QuerySet[Ticket]):
+    bio = io.BytesIO()
+    mow = zipfile.ZipFile(bio, mode="x")
+    for a in queryset:
+        qr = qrmaker.make_qr_code_image(a.code, qr_code_options=qrmaker.QRCodeOptions(
+            size="H",
+            alignment_dark_color="#F00",
+            light_color="#FFFF"
+        ))
+        mow.writestr(f"{a.full_name}.png", qr)
+    mow.close()
+    bio.seek(0)
+    return FileResponse(bio, as_attachment=True, filename="mow.zip")
+
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ("full_name", "category", "cost", "passes", )
+    list_display = ("full_name", "category", "cost", "passes", "order", )
     search_fields = ("full_name", "comments", )
     list_filter = ("category", )
+    actions = [admin.actions.delete_selected, download_stuff]
 
 admin.site.register(Ticket, TicketAdmin)
 admin.site.unregister(Group)
