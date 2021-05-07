c5fdff3b1cf08e9d92f33b5fa68c23d919526778
Vladimir Serov <me@cab404.ru>
2021-05-07 23:20:59 +0300

    added qr to attachments, blackified everything

--- ticket_checker/panel/admin.py
+++ ticket_checker/panel/admin.py
@@ -9,6 +9,7 @@ from django.core.mail import get_connection as mail_sender, EmailMultiAlternativ
 from django.template.loader import render_to_string
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
+
 # Register your models here.
 from .models import Ticket
 
@@ -16,42 +17,80 @@ def download_stuff(modeladmin, request, queryset: QuerySet[Ticket]):
     bio = io.BytesIO()
     mow = zipfile.ZipFile(bio, mode="x")
     for a in queryset:
-        qr = qrmaker.make_qr_code_image(a.code, qr_code_options=qrmaker.QRCodeOptions(
-            size="M",
-            alignment_dark_color="#F00",
-            light_color="#FFFF",
-            image_format="png"
-        ))
+        qr = qrmaker.make_qr_code_image(
+            a.code,
+            qr_code_options=qrmaker.QRCodeOptions(
+                size="M",
+                alignment_dark_color="#F00",
+                light_color="#FFFF",
+                image_format="png",
+            ),
+        )
         fname = f"{str(a.full_name).strip()}-{str(a.code)[:5]}.png"
         mow.writestr(fname, qr)
     mow.close()
     bio.seek(0)
     return FileResponse(bio, as_attachment=True, filename="mow.zip")
 
+
 download_stuff.short_description = _("Download those tickets!")
+
 
 def send_emails(modeladmin, request, queryset: QuerySet[Ticket]):
     conn = mail_sender(fail_silently=False)
+
     def form_mail(ticket):
-        return EmailMultiAlternatives(
+        mail = EmailMultiAlternatives(
             "Билет на кон",
-            f"В этом html-письме ­— твой QR код. Посмотри его в клиенте, поддерживающем html!",
+            f"В этом письме ­— твой QR код для прохода на кон!",
             settings.EMAIL_HOST_USER,
             [ticket.email],
-            connection = conn,
+            connection=conn,
             alternatives=[
-                (render_to_string("panel/email.html", context={"ticket": ticket}), 'text/html')
-            ]
+                (
+                    render_to_string("panel/email.html", context={"ticket": ticket}),
+                    "text/html",
+                ),
+            ],
         )
+        mail.attach(
+            "ticket.png",
+            qrmaker.make_qr_code_image(
+                ticket.code,
+                qrmaker.QRCodeOptions(
+                    size="M",
+                    alignment_dark_color="#F00",
+                    light_color="#FFFF",
+                    image_format="png",
+                ),
+            ),
+            "image/png",
+        )
+        return mail
 
-    mail_sender(fail_silently=False).send_messages([form_mail(ticket) for ticket in queryset])
+    mail_sender(fail_silently=False).send_messages(
+        [form_mail(ticket) for ticket in queryset]
+    )
+
+
 send_emails.short_description = _("Send emails!")
 
+
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ("full_name", "category", "cost", "passes", "order", )
-    search_fields = ("full_name", "comments", )
-    list_filter = ("category", )
+    list_display = (
+        "full_name",
+        "category",
+        "cost",
+        "passes",
+        "order",
+    )
+    search_fields = (
+        "full_name",
+        "comments",
+    )
+    list_filter = ("category",)
     actions = [admin.actions.delete_selected, send_emails, download_stuff]
+
 
 admin.site.register(Ticket, TicketAdmin)
 admin.site.unregister(Group)
